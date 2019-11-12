f2aee83b5ccc4885613c4b5c89cb205c932a2794
albamaran <42845928+albamaran@users.noreply.github.com>
2019-11-12 15:03:20 +0100

    Ajout ext_in

--- VM ipv6/partage/extremite/extremite.c
+++ VM ipv6/partage/extremite/extremite.c
@@ -1,5 +1,6 @@
 #include "extremite.h"
 
+
 void ext_out (){
 
     SOCKADDR_IN sin;
@@ -28,7 +29,7 @@ void ext_out (){
             sin.sin_port = htons(PORT);               /* Port défini dans .h (précisé dans le sujet: 123) */
             sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
 
-            while(1){
+
               if(sock_err != SOCKET_ERROR)
               {
 
@@ -46,26 +47,57 @@ void ext_out (){
               else{
                   perror("bind");
               }
+              while(1){
+                char buffer[1024] = {0};
+                int n = 0;
 
-              char buffer[1024] = {0};
-              int n = 0;
+                if((n = recv(csock, buffer, sizeof(buffer) - 1, 0)) < 0)
+                {
+                    perror("recv()");
+                    exit(0);
+                }
 
-              if((n = recv(csock, buffer, sizeof(buffer) - 1, 0)) < 0)
-              {
-                  perror("recv()");
-                  exit(0);
+                buffer[n] = '\0';
+                write(SORTIE_STANDARD, buffer, sizeof(buffer));
+
               }
-
-              buffer[n] = '\0';
-              write(SORTIE_STANDARD, buffer, sizeof(buffer));
-
-
               closesocket(csock);
-            }
-            closesocket(sock);
+              closesocket(sock);
         }
         else
             perror("socket");
 
     }
 }
+
+void ext_in(int fd){
+  int erreur = 0;
+  SOCKET sock;
+    SOCKADDR_IN sin;
+
+    if(!erreur)
+    {
+        /* Création de la socket */
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+
+        /* Configuration de la connexion */
+        sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(PORT);
+
+        /* Si le client arrive à se connecter */
+        if(connect(sock, (SOCKADDR*)&sin, sizeof(sin)) == SOCKET_ERROR){
+          return;
+        }
+
+        copie_src_dst(fd, sock);
+        /* On ferme la socket précédemment ouverte */
+        closesocket(sock);
+
+    }
+  }
+
+  int main(){
+    ext_out();
+    return 0;
+  }
