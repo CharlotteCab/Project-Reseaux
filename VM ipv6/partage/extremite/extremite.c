88b6de89476760e65826165b4ee7bdced4ca90c1
albamaran <42845928+albamaran@users.noreply.github.com>
2019-11-12 14:14:58 +0100

    Ext_out version question 3.1

--- VM ipv6/partage/extremite/extremite.c
+++ VM ipv6/partage/extremite/extremite.c
@@ -1,26 +1,71 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-void ext_out (struct sockaddr * addr){
-  hSocket = socket( PF_INET, SOCK_DGRAM, 0 );
-  addr.sin_family = AF_INET ;
-  addr.sin_addr.s_addr = htonl (INADDR_ANY);
-  addr.sin_port = htons ((unsigned short)123 ); // Le port d'écoute
-  if ( bind( hSocket, (struct sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR )
-  {
-    printf( "bind() error %d\n", SOCKET_ERRNO );
-    return -1;
-  }
-  char buffer[1024];
-  if((n = recvfrom(sock, buffer, sizeof buffer - 1, 0, (SOCKADDR *)&from, &fromsize)) < 0)
-  {
-    perror("recvfrom()");
-    exit(errno);
-  }
-  buffer[n] = '\0';
-  write(1,buffer, sizeof(buffer));
+#include "extremite.h"
+
+void ext_out (){
+
+    SOCKADDR_IN sin;
+    SOCKET sock;
+    socklen_t recsize = sizeof(sin);
+
+    SOCKADDR_IN csin;
+    SOCKET csock;
+    socklen_t crecsize = sizeof(csin);
+
+    int sock_err;
 
 
+    int erreur = 0;
+
+    if(!erreur)
+    {
+        sock = socket(AF_INET, SOCK_STREAM, 0); //
+
+        if(sock != INVALID_SOCKET)
+        {
+
+            /* Configuration */
+            sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* Adresse IP automatique */
+            sin.sin_family = AF_INET;                 /* AF_INET --> IPv4*/
+            sin.sin_port = htons(PORT);               /* Port défini dans .h (précisé dans le sujet: 123) */
+            sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
+
+            while(1){
+              if(sock_err != SOCKET_ERROR)
+              {
+
+                    sock_err = listen(sock, 5);
+                    if(sock_err != SOCKET_ERROR)
+                    {
+
+                        csock = accept(sock, (SOCKADDR*)&csin, &crecsize);
+
+                    }
+                    else
+                        perror("listen");
+                  }
+                //}
+              else{
+                  perror("bind");
+              }
+
+              char buffer[1024] = {0};
+              int n = 0;
+
+              if((n = recv(csock, buffer, sizeof(buffer) - 1, 0)) < 0)
+              {
+                  perror("recv()");
+                  exit(0);
+              }
+
+              buffer[n] = '\0';
+              write(SORTIE_STANDARD, buffer, sizeof(buffer));
+
+
+              closesocket(csock);
+            }
+            closesocket(sock);
+        }
+        else
+            perror("socket");
+
+    }
 }
-
-void ext_in();
